a613ec9a93859c50ec1c28f11b0d954a5277afe6
Arjan18 <b7020246@my.shu.ac.uk>
2021-03-29 14:10:35 +0100

    CHanges

--- routes/index.js
+++ routes/index.js
@@ -29,4 +29,28 @@ router.get('/profile', ensureAuthenticated, (req, res) =>
   })
 );
 
+router.get('/FilmList', ensureAuthenticated, (req, res) =>
+  res.render('FilmList', {
+    user: req.user
+  })
+);
+
+router.get('/films', ensureAuthenticated, (req, res) =>
+  res.render('films', {
+    user: req.user
+  })
+);
+
+router.get('/ShowList', ensureAuthenticated, (req, res) =>
+  res.render('ShowList', {
+    user: req.user
+  })
+);
+
+router.get('/shows', ensureAuthenticated, (req, res) =>
+  res.render('shows', {
+    user: req.user
+  })
+);
+
 module.exports = router;
